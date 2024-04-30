90cf93ac519f2d5fcf78becdaa73bea3d269192d
Kevin Do <91227948+KevintrDo@users.noreply.github.com>
2024-04-30 04:23:08 -0400

    API cat

--- 4020/CatAPI/kitty.js
+++ 4020/CatAPI/kitty.js
@@ -14,13 +14,15 @@ new Vue({
                 axios.defaults.headers.common['x-api-key'] = "5707ff43-c0b5-456f-864a-78a03c24ea46"
                 let response = await axios.get('https://api.thecatapi.com/v1/breeds');
                 this.catArray = response.data;
+                console.log("UUP");
+                console.log(response.data[1].name);
+                console.log(response.data[1].affection_level);
             } catch (err) {
                 console.log(err)
             }
-
         },
-        barPosition(level) {
-            return 20 * level;
+        computedBars(level) {
+            return level;
         }
     }
 })
